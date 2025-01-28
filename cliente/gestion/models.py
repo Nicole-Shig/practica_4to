814cc7877add86a9ee8d80cccadeaaed5b53fa72
Nicole-Shig <nicole.shiguango9413@utc.edu.ec>
2025-01-28 10:48:39 -0500

    practic

---
+++ cliente/gestion/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class cliente (models.Model):
+    cedula = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=10)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return f"su cedula es - {self.cedula} -- {self.nombre} -- {self.apellido}"
---
+++ cliente/interfaz.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import messagebox
+
+#construir mi ventana
+
+ventana = tk.Tk()
+ventana.title('sistema de registro de clientes'),
+ventana.geometry('300x200')    
+ventana.mainloop()
+
+#crear variables
